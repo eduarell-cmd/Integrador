35b28f6ac88fe84eb601831c6642eae5b27edd89
iAyden <epicliam19@hotmail.com>
2024-11-17 16:36:24 -0600

    Admin

---
+++ admin.py
@@ -0,0 +1,16 @@
+from conexionsql import connection
+import logging
+def get_admin_by_id(session_id):
+    cursor = connection.cursor()
+    query="SELECT ID_Admin FROM Admin WHERE Persona_ID_Persona = ?"
+    try:
+        cursor.execute(query, (session_id))
+        admin = cursor.fetchone()
+        if not admin:
+            print("Este usuario no es administrador")
+            return False
+        return admin
+    except Exception as e:
+        logging.error(f"Error durante la busqueda de un administrador:{e}")
+    finally:
+            connection.close()
