8d671f32fb0603a452aeaa697da7f63947485649
iAyden <epicliam19@hotmail.com>
2024-11-27 22:51:29 -0600

    Jalando wpp

--- seller.py
+++ seller.py
@@ -256,7 +256,8 @@ def get_sellerinfo_by_user_id(user_id):
         cursor.execute(query, (user_id,))
         
         result = cursor.fetchone()
-        print(f"Telefono:{result}")
+        posicion0 = result[0]
+        print(f"Telefono:{posicion0}")
         if result:
             return result
         return None
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,12 @@
-@app.route('/locationvp', methods=['GET'])
-def ubicacion_pv():
-    
-
-        # Obtener el índice del producto desde el parámetro de consulta
-    result = exec_mostar_tienda()
-    productid_str = request.args.get('productid')# Por defecto, 0
-    productid = int(productid_str)
-    producto_seleccionado = result[productid]
-    pointid = producto_seleccionado['IDPV']
-    print(f"Pointid:{pointid}")
-    intpointid = int(pointid)
-    
-    products = get_products_by_point_id(intpointid)
-
-    # Pasar todos los productos y el producto seleccionado al template
-    return render_template('seller_location.html',Producto=producto_seleccionado,productseller=products)
+@app.route('/logout')
+def logout():
+    ID_Persona = session.get('user_id')
+    if not ID_Persona:
+        print("Aqui falla /logout")
+        return redirect(url_for('login'))
+    user = get_user_by_email(ID_Persona)
+    email = user['email'] if user else None
+    session.pop('user_id', None)
+    session.clear()
+    flash("Has cerrado sesión exitosamente", "info")
+    return redirect(url_for('index'))     
